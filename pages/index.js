50d59f6447391b9f86d546e729bf73ed64905675
gideonmandu <gideonmandu@gmail.com>
2021-12-05 17:05:34 +0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify 2.0</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
+      <div>
+        {/* player */}
+      </div>
+
+    </div>
+  )
+}
